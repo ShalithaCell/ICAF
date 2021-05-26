65ad3f67edca180d116fd7eecd17b2662f9878d9
1001postidal <84646153+1001postidal@users.noreply.github.com>
2021-05-26 22:44:57 +0530

    configure authentication

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,11 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const KoaStatic = require('koa-static');
+const cors = require('@koa/cors');
+const logger = require('koa-logger');
+// const loggerFile = require('koa-logging-middleware');
 const router = require('./api');
-const { dbContext, exceptionService } = require('./services');
+const { dbContext, exceptionService, applicationDataSeeder } = require('./services');
 
 // init the database connection.
 dbContext();
@@ -12,9 +16,27 @@ app
     .use(koaBody())
     .use(exceptionService.errorHandler) // register generic error handler middleware
     .use(exceptionService.jsonErrorHandler) // register json error handler middleware
+    .use(cors()) // allowed CORS
     .use(router()) // Use the Router on the sub routes
+    .use(KoaStatic('public')) // server statics
+    .use(logger()) // enable logs
+    // .use(loggerFile('./log/')) // log to file
     // Bootstrap the server
     .listen(process.env.PORT || 5000, () =>
     {
         console.log('server stared with port 5000');
+
+        // application default data seeder
+        applicationDataSeeder.seedRoles()
+            .then((r) => console.info('role data seeder executed'));
+        applicationDataSeeder.seedUsers()
+            .then((r) => console.info('user data seeder executed'));
+
+        console.log();
+        console.log();
+        console.log('=======================Server StartUp===========================');
+        console.log('For more info :');
+        console.log('\x1b[33m\x1b[4m%s\x1b[0m', 'http://localhost:5000/api/');
+        console.log('=====================================================================');
+        console.log();
     });
--- server/src/services/user/user.service.js
+++ server/src/services/user/user.service.js
@@ -1,14 +1,119 @@
 const { User } = require("../../models");
+const { NewUser } = require('../../types');
+const RoleService = require('../userRole/userRole.service');
 
-const getUserFromDb = async (email, password) =>
-{
-    const data = await User.find({});
+const UserService = {
+    find : async (email, password) =>
+    {
+        const data = await User.findOne({ email, password }).populate('role');
 
-    console.log(data);
+        return data;
+    },
+    findByEmail : async (email) =>
+    {
+        const data = await User.findOne({ email }).populate('role').catch((err) =>
+        {
+            // TODO: log error
+            console.log(err);
 
-    return data;
+            return null;
+        });
+
+        return data;
+    },
+    create : async (userData, seedingStatus) =>
+    {
+        try
+        {
+            // check data validation
+            const request = Object.setPrototypeOf(userData, NewUser.prototype);
+
+            if (!request.isValid())
+            {
+                return null;
+            }
+            // check already exists
+            const users = await UserService.findByEmail(request.email);
+
+            // check role
+            const role = await RoleService.find(request.role);
+
+            if (!role)
+            {
+                return {
+                    created : false,
+                    message : `Role does not exists. Please check selected role again.`,
+                };
+            }
+
+            if (users)
+            {
+                return {
+                    created : false,
+                    message : `this email address is already in use.`,
+                };
+            }
+
+            const user = new User({
+                name           : request.name,
+                email          : request.email,
+                password       : request.password,
+                phone          : request.phone,
+                emailConfirmed : seedingStatus,
+                role           : role._id,
+            });
+
+            // create user
+            const data = await user.save();
+
+            return {
+                created : true,
+                data,
+            };
+        }
+        catch (e)
+        {
+            console.log(e);
+            throw e;
+        }
+    },
+    updateUser : async (email, name, phone) =>
+    {
+        await User.updateOne({ email }, {
+            name,
+            phone,
+        }, (err, affected, resp) =>
+        {
+            console.log(resp);
+        });
+
+        return UserService.findByEmail(email);
+    },
+    activeAccount : async (email) =>
+    {
+        User.updateOne({ email }, {
+            emailConfirmed : true,
+        }, (err, affected, resp) =>
+        {
+            console.log(resp);
+        });
+    },
+    resetPassword : async (email, password) =>
+    {
+        try
+        {
+            User.updateOne({ email }, {
+                password,
+            }, (err, affected, resp) =>
+            {
+                console.log(resp);
+            });
+        }
+        catch (e)
+        {
+            console.log(e);
+        }
+    },
 };
 
-module.exports = {
-    getUserFromDb,
-};
+module.exports = UserService;
