47aa26c4b2d151079706ca8c8772d26bcb23bf28
shalitha <shalithax@gmail.com>
2021-05-30 10:34:43 +0530

    implement user functions

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -2,10 +2,12 @@ const combineRouters = require('koa-combine-routers');
 // auth routes
 const authRouter = require('./v1/auth/authRouter');
 const applicationRouter = require('./application/applicationRouter');
+const userRouter = require('./v1/user/userRouter');
 
 const router = combineRouters(
     applicationRouter,
     authRouter,
+    userRouter,
 );
 
 module.exports = router;
--- server/src/services/index.js
+++ server/src/services/index.js
@@ -4,6 +4,9 @@ const userService = require('./user/user.service');
 const exceptionService = require('./exception/exception.service');
 const dataManagerService = require('./system/dataManager/dataManager.service');
 const RoleService = require('./userRole/userRole.service');
+const TokenService = require('./user/token.service');
+const mailService = require('./system/mailer/nodeMailer.service');
+const emailNotificationService = require('./system/notification/emailNotification.service');
 
 module.exports = {
     dbContext,
@@ -12,4 +15,7 @@ module.exports = {
     exceptionService,
     dataManagerService,
     RoleService,
+    TokenService,
+    mailService,
+    emailNotificationService,
 };
