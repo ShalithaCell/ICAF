3fdd86dd783514e20baaa838cf105df7b275e8f5
shalitha <shalithax@gmail.com>
2021-05-31 19:30:22 +0530

    resolved test case issues

--- server/src/__tests__/route/routes.test.js
+++ server/src/__tests__/route/routes.test.js
@@ -16,9 +16,14 @@ afterAll(async () =>
     console.log('server closed!');
 });
 
+afterEach(async () =>
+{
+    await server.close();
+});
+
 describe('When testing the server.js', () =>
 {
-    const agent = request.agent(server);
+    // const agent = request.agent(server);
 
     it('Should connect successfully and be able to return a response',
         async () => new Promise((resolve) =>
@@ -39,7 +44,7 @@ describe('When testing the server.js', () =>
 
 describe('When testing the login', () =>
 {
-    const agent = request.agent(server);
+    // const agent = request.agent(server);
 
     it('responds with json', async () => new Promise((resolve) =>
     {
@@ -55,6 +60,88 @@ describe('When testing the login', () =>
 
                 expect(res.body.success).toBe(true);
 
+                resolve();
+            });
+    }), 30000);
+});
+
+describe('When testing the user create', () =>
+{
+    // const agent = request.agent(server);
+
+    it('responds with json', async () => new Promise((resolve) =>
+    {
+        const randomEmail = `test123456${Date.now()}@gmail.com`;
+
+        request(server)
+            .post('/api/v1/user/')
+            .send({
+                name     : 'testingUser',
+                password : "Shalitha@123456",
+                email    : randomEmail,
+                phone    : "0711234567",
+                role     : "user" })
+            .set('Accept', 'application/json')
+            .retry(2)
+            .expect('Content-Type', /json/)
+            .end((err, res) =>
+            {
+                expect(res.status).toBe(200);
+
+                expect(res.body.success).toBe(true);
+                expect(res.body.message.includes(randomEmail)).toBe(true);
+                resolve();
+            });
+    }), 30000);
+});
+
+describe('When testing the user create', () =>
+{
+    // const agent = request.agent(server);
+
+    it('responds with json', async () => new Promise((resolve) =>
+    {
+        const randomEmail = `test123456${Date.now()}@gmail.com`;
+
+        request(server)
+            .post('/api/v1/user/')
+            .send({
+                name     : 'testingUser',
+                password : "Shalitha@123456",
+                email    : randomEmail,
+                phone    : "0711234567",
+                role     : "user" })
+            .set('Accept', 'application/json')
+            .retry(2)
+            .expect('Content-Type', /json/)
+            .end((err, res) =>
+            {
+                expect(res.status).toBe(200);
+
+                expect(res.body.success).toBe(true);
+                expect(res.body.message.includes(randomEmail)).toBe(true);
+                resolve();
+            });
+    }), 30000);
+});
+
+
+describe('When testing the role fetching', () =>
+{
+    // const agent = request.agent(server);
+
+    it('responds with json', async () => new Promise((resolve) =>
+    {
+        request(server)
+            .get('/api/v1/role/')
+            .set('Accept', 'application/json')
+            .retry(2)
+            .expect('Content-Type', /json/)
+            .end((err, res) =>
+            {
+                expect(res.status).toBe(200);
+
+                expect(res.body.success).toBe(true);
                 resolve();
             });
     }), 30000);
--- server/src/api/index.js
+++ server/src/api/index.js
@@ -3,11 +3,13 @@ const combineRouters = require('koa-combine-routers');
 const authRouter = require('./v1/auth/authRouter');
 const applicationRouter = require('./application/applicationRouter');
 const userRouter = require('./v1/user/userRouter');
+const roleRouter = require('./v1/role/roleRouter');
 
 const router = combineRouters(
     applicationRouter,
     authRouter,
     userRouter,
+    roleRouter,
 );
 
 module.exports = router;
