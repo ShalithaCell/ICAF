53c707fa7f712d3ac43bcb260a00ec97fe540f82
Dulanji1 <mddulanjicooray@gmail.com>
2021-06-26 23:31:13 +0530

    ResearchPaper Upload,View , NavBar, Workshop Upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { Login, Home } from './scenes/index';
+import { Login, Home, ResearchPaperPublish,ResearchPaperView,ResearchWorkShop,ResearchWorkShopView,CallForResearch} from './scenes/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/navbar/navbar.component';
 
 function App()
 {
 	return (
 		<Router>
 			<div>
+				<NavbarComponent />
 				<Switch>
 					<Route exact path='/' component={Home} />
 					<Route path='/login' component={Login} />
+					<Route path='/research/publish' component={ResearchPaperPublish} />
+					<Route path='/research/view' component={ResearchPaperView} />
+					<Route path='/researchWorkshop/publish' component={ResearchWorkShop} />
+					<Route path='/researchWorkshopView/view' component={ResearchWorkShopView} />
+					<Route path='/researchWorkshopCall' component={CallForResearch} />
+
 				</Switch>
 			</div>
 		</Router>
