1743e30cb6ee21e54d9259c0deaed231130ebb52
shalitha <shalithax@gmail.com>
2021-06-27 00:36:53 +0530

    Re change routing architecture

--- client/src/components/molecules/mainNavbar/mainNavbar.jsx
+++ client/src/components/molecules/mainNavbar/mainNavbar.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { AppBar, Toolbar } from '@material-ui/core';
-import logo from '../../../assets/images/logo.png';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-const MainNavbar = (props) => (
-	<AppBar
-		elevation={0}
-		{...props}
-	>
-		<Toolbar sx={{ height: 64 }}>
-			<RouterLink to='/'>
-				<img
-					alt='Logo'
-					src={logo}
-					{...props}
-				/>
-			</RouterLink>
-		</Toolbar>
-	</AppBar>
-);
+const MainNavbar = (props) =>
+{
+	const navigate = useNavigate();
+
+	return (
+		<AppBar
+			elevation={0}
+			{...props}
+		>
+			<Toolbar sx={{ height: 64 }}>
+				<Toolbar>
+					<IconButton
+						size='large'
+						edge='start'
+						color='inherit'
+						aria-label='menu'
+						sx={{ mr: 2 }}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+						News
+					</Typography>
+					<Button color='inherit' onClick={() => navigate('/login')}>Login</Button>
+					<Button color='inherit' onClick={() => navigate('/researchWorkshop/publish')}>Workshop</Button>
+					<Button color='inherit' onClick={() => navigate('/researchWorkshopView/view')}>View</Button>
+					<Button color='inherit' onClick={() => navigate('/researchWorkshopCall/view')}>Call For Workshop</Button>
+					<Button color='inherit' onClick={() => navigate('/research/publish')}>Publish</Button>
+					<Button color='inherit' onClick={() => navigate('/research/view')}>View</Button>
+					<Button color='inherit' onClick={() => navigate('/research/templates')}>Templates</Button>
+
+				</Toolbar>
+			</Toolbar>
+		</AppBar>
+	);
+};
 
 export default MainNavbar;
--- client/src/navigations/index.js
+++ client/src/navigations/index.js
@@ -3,7 +3,16 @@ import { Navigate } from 'react-router-dom';
 import SettingsView from "../scenes/dashboard/settings/settings.component";
 import DashboardLayout from "../components/organisms/dashboard/dashboardLayout";
 import MainLayout from "../components/molecules/mainLayout/mainLayout";
-import { Home, Login, NotFound } from '../scenes';
+import {
+	Home,
+	Login,
+	NotFound,
+	CallForResearch,
+	ResearchPaperPublish,
+	ResearchPaperView,
+	ResearchWorkShop,
+	ResearchWorkShopView,
+} from '../scenes';
 
 const routes = [
 	{
@@ -24,6 +33,12 @@ const routes = [
 		children : [
 			{ path: 'login', element: <Login /> },
 			{ path: 'register', element: <Login /> },
+			{ path: '/researchWorkshop/publish', element: <ResearchWorkShop /> },
+			{ path: '/researchWorkshopView/view', element: <ResearchWorkShopView /> },
+			{ path: '/researchWorkshopCall/view', element: <CallForResearch /> },
+			{ path: '/research/publish', element: <ResearchPaperPublish /> },
+			{ path: '/research/view', element: <ResearchPaperView /> },
+			{ path: '/research/templates', element: <Login /> },
 			{ path: '404', element: <NotFound /> },
 			{ path: '/', element: <Home /> },
 			{ path: '*', element: <Navigate to='/404' /> },
