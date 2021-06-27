d7a664a58b1c6b5b31fe60e59c011712a49e513a
Dulanji1 <mddulanjicooray@gmail.com>
2021-06-27 22:15:31 +0530

    Update Nav Bar, Workshop tamplates2

---
+++ client/src/scenes/researchpapertamplets/researchPaperTamplets.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+
+export default function ResearchPaperTamplets()
+{
+	return (
+		<div className='container'>
+			<div className="container">
+				<h2>Home</h2>
+				<div className="alert alert-secondary" role="alert"
+
+					 style={{
+						 height:360,
+					 }}
+				>
+
+				</div>
+				<div className="alert alert-primary" role="alert"
+
+				>
+
+				</div>
+
+				<div className="alert alert-danger" role="alert">
+
+				</div>
+				<div className="alert alert-warning" role="alert">
+
+				</div>
+				<div className="alert alert-info" role="alert">
+
+				</div>
+
+				<div className="alert alert-dark" role="alert">
+
+				</div>
+			</div>
+		</div>
+	);
+}
