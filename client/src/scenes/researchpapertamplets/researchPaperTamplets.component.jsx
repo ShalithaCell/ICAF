fe3371f4ad92ea9494f082ccd9f73e8483c5e884
shalitha <shalithax@gmail.com>
2021-06-27 22:27:58 +0530

    set conflict

--- client/src/scenes/researchpapertamplets/researchPaperTamplets.component.jsx
+++ client/src/scenes/researchpapertamplets/researchPaperTamplets.component.jsx
@@ -1,40 +1,26 @@
 import React from 'react';
 
-
-
-export default function ResearchPaperTamplets()
+export default function ResearchPaperTemplates()
 {
 	return (
 		<div className='container'>
-			<div className="container">
+			<div className='container'>
 				<h2>Home</h2>
-				<div className="alert alert-secondary" role="alert"
-
+				<div
+					className='alert alert-secondary' role='alert'
 					 style={{
-						 height:360,
+						 height : 360,
 					 }}
-				>
+				/>
+				<div
+					className='alert alert-primary' role='alert'
+				/>
 
-				</div>
-				<div className="alert alert-primary" role="alert"
+				<div className='alert alert-danger' role='alert' />
+				<div className='alert alert-warning' role='alert' />
+				<div className='alert alert-info' role='alert' />
 
-				>
-
-				</div>
-
-				<div className="alert alert-danger" role="alert">
-
-				</div>
-				<div className="alert alert-warning" role="alert">
-
-				</div>
-				<div className="alert alert-info" role="alert">
-
-				</div>
-
-				<div className="alert alert-dark" role="alert">
-
-				</div>
+				<div className='alert alert-dark' role='alert' />
 			</div>
 		</div>
 	);
