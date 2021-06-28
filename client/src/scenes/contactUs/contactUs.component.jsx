21f08b80545fa4f030211bd003f089402c5f131c
Hashinthi Deshni <hashinthideshni555@gmail.com>
2021-06-28 11:05:36 +0530

    Create ContactUs page

--- client/src/scenes/contactUs/contactUs.component.jsx
+++ client/src/scenes/contactUs/contactUs.component.jsx
@@ -6,11 +6,18 @@ export default function ContactUs()
 {
 	return (
 		<div className='container'>
-			<img src='../../assets/images/aboutUs.jpg' className='img-fluid' alt='Responsive image' />
+			<h1>Contact Us Page</h1>
+			{/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+			<img src='https://media.istockphoto.com/photos/business-network-concept-customer-support-shaking-hands-picture-id1256603011?s=612x612' width='800' height='300' className='img-fluid' alt='' />
 			<form>
+				<h2>Leave a Comment</h2>
+				<Form.Group controlId="exampleForm.ControlInput1">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control type="email" placeholder="name@example.com" />
+				</Form.Group>
 				<div className='mb-3'>
 
-					<label htmlFor='subjectName' className='form-label'>Title</label>
+					<label htmlFor='subjectName' className='form-label'>Name</label>
 					<input
 						type='text'
 						className='form-control'
@@ -18,9 +25,9 @@ export default function ContactUs()
 						name='subjectName'
 					/>
 				</div>
-
 				<div className='mb-3'>
-					<label htmlFor='subjectName' className='form-label'>Publisher Name</label>
+
+					<label htmlFor='subjectName' className='form-label'>Subject</label>
 					<input
 						type='text'
 						className='form-control'
@@ -28,45 +35,13 @@ export default function ContactUs()
 						name='subjectName'
 					/>
 				</div>
-
-				<div className='mb-3'>
-					<label htmlFor='description' className='form-label'>Description</label>
-					<textarea
-						className='form-control'
-						id='description'
-						rows='3'
-					/>
-					<br/>
-
-					<div className='mb-3'>
-						<label htmlFor='subjectName' className='form-label'>Research Paper</label>
-					</div>
-
-					<Form.File
-						className='form-control'
-						id="datetime"
-						data-browse="Bestand kiezen"
-						custom
-					/>
-
-				</div>
-				<div className='mb-3'>
-					<label htmlFor='subjectAmount' className='form-label'>Publish Date</label>
-					<br/>
-
-					<input
-						type="datetime-local"
-						className='form-control'
-						id='subjectName'
-						name='subjectName'
-					/>
-
-				</div>
-
-				<br/>
+				<Form.Group controlId="exampleForm.ControlTextarea1">
+					<Form.Label>Youre Comment(Optional) </Form.Label>
+					<Form.Control as="textarea" rows={3} />
+				</Form.Group>
+				<br />
 				<button type='submit' className='btn btn-primary'>Submit</button>
 			</form>
-			<img src='../../assets/images/aboutUs.jpg' className='img-fluid' />
 		</div>
 	);
 }
