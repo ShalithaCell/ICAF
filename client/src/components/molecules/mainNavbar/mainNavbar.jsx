dc394d00d7be3fa3f5dc293d88417c25cba9f60f
Hashinthi Deshni <hashinthideshni555@gmail.com>
2021-06-28 15:03:33 +0530

    Add group list to user guide and change navbar

--- client/src/components/molecules/mainNavbar/mainNavbar.jsx
+++ client/src/components/molecules/mainNavbar/mainNavbar.jsx
@@ -16,54 +16,121 @@ const MainNavbar = (props) =>
 			elevation={0}
 			{...props}
 		>
-			<Toolbar sx={{ height: 64 }}>
-				<Toolbar>
+			{/* <Toolbar sx={{ height: 64 }}>*/}
+			{/*	<Toolbar>*/}
+			{/*		<IconButton*/}
+			{/*			size='large'*/}
+			{/*			edge='start'*/}
+			{/*			color='inherit'*/}
+			{/*			aria-label='menu'*/}
+			{/*			sx={{ mr: 2 }}*/}
+			{/*		>*/}
+			{/*			<MenuIcon />*/}
+			{/*		</IconButton>*/}
+			{/*		<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>*/}
+			{/*			News*/}
+			{/*		</Typography>*/}
+			{/* <Button color='inherit' onClick={() => navigate('/login')}>Login</Button>*/}
+			{/* <Button color='inherit' onClick={() => navigate('/researchWorkshop/publish')}>Workshop</Button>*/}
+			{/* <Button color='inherit' onClick={() => navigate('/researchWorkshopView/view')}>View</Button>*/}
+			{/* <Button color='inherit' onClick={() => navigate('/researchWorkshopCall/view')}>Call For Workshop</Button>*/}
+			{/* <Button color='inherit' onClick={() => navigate('/research/publish')}>Publish</Button>*/}
+			{/* <Button color='inherit' onClick={() => navigate('/research/view')}>View</Button>*/}
+			{/* <Button color='inherit' onClick={() => navigate('/research/templates')}>Templates</Button>*/}
+
+			{/* <NavDropdown  title="Research Workshop" id="navbarScrollingDropdown" className="text-white">*/}
+			{/*	<NavDropdown.Item href="/researchWorkshop/publish">Workshop </NavDropdown.Item>*/}
+			{/*	<NavDropdown.Item href="/researchWorkshopView/view">View</NavDropdown.Item>*/}
+			{/*	<NavDropdown.Divider />*/}
+			{/*	<NavDropdown.Item href="/researchWorkshopCall">Call For Workshop</NavDropdown.Item>*/}
+			{/* </NavDropdown>*/}
+
+			{/* <NavDropdown  title="Research Papers" id="navbarScrollingDropdown">*/}
+			{/*	<NavDropdown.Item href="/research/publish">Publish</NavDropdown.Item>*/}
+			{/*	<NavDropdown.Item href="/research/view">View</NavDropdown.Item>*/}
+			{/*	<NavDropdown.Divider />*/}
+			{/*	<NavDropdown.Item href="/research/templates">Templates</NavDropdown.Item>*/}
+			{/* </NavDropdown>*/}
+
+			<nav className='navbar navbar-expand-lg navbar-light bg-secondary'>
+				<div className='container-fluid'>
 					<img
 						src='https://i.pinimg.com/originals/31/8d/a3/318da3082926e54402950eaf0d7f68ba.png'
 						width='30' height='30' className='d-inline-block align-top' alt=''
 					/>
 
-					<li className='nav-item'>
-						<a className='nav-link active' aria-current='page' href='/'>Home |</a>
-					</li>
-					<li className='nav-item'>
-						<a className='nav-link  text-dark' href='/userGuide'>User Guide |</a>
-					</li>
-					<li className='nav-item'>
-						<a className='nav-link  text-dark'>About Us |</a>
-					</li>
-					<li className='nav-item'>
-						<a className='nav-link  text-dark' href='/contactUs'>Contact Us |</a>
-					</li>
-					<li className='nav-item'>
-						<a className='nav-link active' aria-current='page' href='/'>News |</a>
-					</li>
+					<a className='navbar-brand text-info' href='#'>ICAF</a>
 
-					<li className='nav-item'>
-						<a className='nav-link text-dark' href='/login'>Login |</a>
-					</li>
+					<button
+						className='navbar-toggler' type='button' data-bs-toggle='collapse'
+						data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false'
+						aria-label='Toggle navigation'
+					>
+						<span className='navbar-toggler-icon' />
+					</button>
+					<div className='collapse navbar-collapse' id='navbarNav'>
+						<ul className='navbar-nav'>
+							<li className='nav-item'>
+								<a className='nav-link active' aria-current='page' href='/'>Home |</a>
+							</li>
+							<li className='nav-item'>
+								<a className='nav-link  text-dark'>About Us |</a>
+							</li>
+							<li className='nav-item'>
+								<a className='nav-link  text-dark' href='/contactUs'>Contact Us |</a>
+							</li>
+							<li className='nav-item'>
+								<a className='nav-link  text-dark' href='/userGuide'>User Guide |</a>
+							</li>
+							<li className='nav-item'>
+								<a className='nav-link active' aria-current='page' href='/'>News |</a>
+							</li>
 
-					<li className='nav-item'>
-						<a className='nav-link text-dark'>Download Materials |</a>
-					</li>
+							<li className='nav-item'>
+								<a className='nav-link text-dark' href='/login'>Login |</a>
+							</li>
 
-					<NavDropdown title='Research Workshop' id='navbarScrollingDropdown'>
-						<NavDropdown.Item href='/researchWorkshop/publish'>Workshop </NavDropdown.Item>
-						<NavDropdown.Item href='/researchWorkshopView/view'>View</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href='/researchWorkshopCall'>Call For Workshop</NavDropdown.Item>
-					</NavDropdown>
+							<li className='nav-item'>
+								<a className='nav-link text-dark'>Download Materials |</a>
+							</li>
 
-					<NavDropdown title='Research Papers' id='navbarScrollingDropdown'>
-						<NavDropdown.Item href='/research/publish'>Publish</NavDropdown.Item>
-						<NavDropdown.Item href='/research/view'>View</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href='/research/templates'>Templates</NavDropdown.Item>
-					</NavDropdown>
+							<NavDropdown title='Research Workshop' id='navbarScrollingDropdown'>
+								<NavDropdown.Item href='/researchWorkshop/publish'>Workshop </NavDropdown.Item>
+								<NavDropdown.Item href='/researchWorkshopView/view'>View</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href='/researchWorkshopCall'>Call For Workshop</NavDropdown.Item>
+							</NavDropdown>
 
+							<NavDropdown title='Research Papers' id='navbarScrollingDropdown'>
+								<NavDropdown.Item href='/research/publish'>Publish</NavDropdown.Item>
+								<NavDropdown.Item href='/research/view'>View</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href='/research/templates'>Templates</NavDropdown.Item>
+							</NavDropdown>
+							<li className='nav-item'>
+								<form
+									style={{
+										marginLeft : 300,
+									}}
+								>
+									 <input
+										className='float-right p-2 bg-light border' type='search'
+										   placeholder='Search' aria-label='Search'
+									 />
+									<button className='btn btn btn-dark mb-2' type='submit'>Search</button>
 
-				</Toolbar>
-			</Toolbar>
+								</form>
+							</li>
+						</ul>
+
+					</div>
+
+				</div>
+
+			</nav>
+
+			{/*	</Toolbar>*/}
+			{/* </Toolbar>*/}
 		</AppBar>
 	);
 };
