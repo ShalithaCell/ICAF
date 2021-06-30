573d3ea90855c20856decee3abc7a2bcf024f878
Hashinthi Deshni <hashinthideshni555@gmail.com>
2021-06-30 21:12:37 +0530

    change research workshop componen review

--- client/src/scenes/workshop/researcWorkshop.component.jsx
+++ client/src/scenes/workshop/researcWorkshop.component.jsx
@@ -22,6 +22,46 @@ export default function ResearchWorkShop()
 						name='subjectName'
 					/>
 				</div>
+				<div className='mb-3'>
+
+					<label htmlFor='subjectName' className='form-label'>Workshop Title</label>
+					<input
+						type='text'
+						className='form-control'
+						id='subjectName'
+						name='subjectName'
+					/>
+				</div>
+				<div className='mb-3'>
+
+					<label htmlFor='subjectName' className='form-label'>Workshop Title</label>
+					<input
+						type='text'
+						className='form-control'
+						id='subjectName'
+						name='subjectName'
+					/>
+				</div>
+				<div className='mb-3'>
+
+					<label htmlFor='subjectName' className='form-label'>Workshop Title</label>
+					<input
+						type='text'
+						className='form-control'
+						id='subjectName'
+						name='subjectName'
+					/>
+				</div>
+				<div className='mb-3'>
+
+					<label htmlFor='subjectName' className='form-label'>Workshop Title</label>
+					<input
+						type='text'
+						className='form-control'
+						id='subjectName'
+						name='subjectName'
+					/>
+				</div>
 
 
 				<div className='mb-3'>
